a8d0f25fea7a4c81865a0dd0c7bccb791e477f50
tan renhui <914162692@qq.com>
2013-03-23 00:55:12 +0800

    init
    init AgileEAP-BPM

---
+++ Presentation/AgileEAP.Web/Plugins/Workflow/WebApiConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using System.Web.Mvc;
+using System.Web.Http;
+using System.Web.Routing;
+using System.Web.Http.Tracing;
+
+using Autofac;
+using Autofac.Configuration;
+using Autofac.Integration.WebApi;
+using AgileEAP.Core;
+using AgileEAP.MVC.Routes;
+using AgileEAP.MVC.Localization;
+using AgileEAP.MVC.WebApi;
+using AgileEAP.Workflow.Engine;
+
+namespace AgileEAP.Plugin.Workflow
+{
+    public class WebApiConfig
+    {
+        public static void Register()
+        {
+            var config = GlobalConfiguration.Configuration;
+
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<TokenAuthentication>().As<ITokenAuthentication>().SingleInstance();
+            builder.RegisterType<WorkflowEngine>().As<IWorkflowEngine>().InstancePerApiRequest();
+
+            var container = builder.Build();
+            var resolver = new AutofacWebApiDependencyResolver(container);
+            config.MessageHandlers.Add(new WebApiAuthenticationHandler(container.Resolve<ITokenAuthentication>()));
+            config.DependencyResolver = resolver;
+          //  builder.RegisterWebApiFilterProvider(config);
+            config.Routes.MapHttpRoute(name: "Workflow.Web.Api",
+                routeTemplate: "workflow/api/{action}",
+                defaults: new { controller = "WorkflowApi", id = RouteParameter.Optional });
+
+            TraceConfig.Register(config);
+        }
+    }
+}
